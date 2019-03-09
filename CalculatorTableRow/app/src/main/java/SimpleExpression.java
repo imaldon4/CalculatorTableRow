1d7cb6c950ea4d731ab25e65cfb8e7ad60da696b
imaldon4 <imaldon4@mail.ccsf.edu>
2019-03-08 17:28:15 -0800

    Finished class

--- CalculatorTableRow/app/src/main/java/SimpleExpression.java
+++ CalculatorTableRow/app/src/main/java/SimpleExpression.java
@@ -12,4 +12,54 @@ public class SimpleExpression {
         mOperator = "+";
         mValue = 0;
     }
+
+    public void setOperand1(int v){
+        mOperand1 = v;
+    }
+
+    public int getOperand1(){
+        return mOperand1;
+    }
+
+    public void setOperand2(int v){
+        mOperand2 = v;
+    }
+
+    public int getOperand2(){
+        return mOperand2;
+    }
+
+    public void setOperator(String s){
+        mOperator = s;
+    }
+
+    public String getOperator(){
+        return mOperator;
+    }
+
+    public int getValue(){
+        computeValue();
+        return mValue;
+    }
+
+    public void clearOperands(){
+        mOperand1 = 0;
+        mOperand2 = 0;
+    }
+
+    private double computeValue(){
+        mValue = 0;
+
+        if (mOperator.contentEquals("+")){
+            mValue = mOperand1 + mOperand2;
+        }else if (mOperator.contentEquals("-")){
+            mValue = mOperand1 - mOperand2;
+        }else if (mOperator.contentEquals("x")){
+            mValue = mOperand1 * mOperand2;
+        }else if (mOperator.contentEquals("/") && mOperand2 != 0){
+            mValue = mOperand1 / mOperand2;
+        }else {
+            mValue = mOperand1 % mOperand2;
+        }
+    }
 }
