21ef970ee4b970feb827ca5d3bd15dcc365a2a1f
imaldon4 <imaldon4@mail.ccsf.edu>
2019-03-08 17:17:39 -0800

    Finished formatting XML files

---
+++ CalculatorTableRow/app/src/main/java/SimpleExpression.java
@@ -0,0 +1,15 @@
+
+
+public class SimpleExpression {
+    private Integer mOperand1;
+    private Integer mOperand2;
+    private String mOperator;
+    private Integer mValue;
+
+    public SimpleExpression(){
+        mOperand1 = 0;
+        mOperand2 = 0;
+        mOperator = "+";
+        mValue = 0;
+    }
+}
